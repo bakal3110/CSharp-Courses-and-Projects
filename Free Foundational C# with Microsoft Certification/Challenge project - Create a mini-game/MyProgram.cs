350f361cd3cf7e9c8baa1a3249687d968e93c501
Bartosz Kaluski <125510397+bakal3110@users.noreply.github.com>
2025-07-26 16:55:18 +0200

    Updated functionality of few methods

--- Free Foundational C# with Microsoft Certification/Challenge project - Create a mini-game/MyProgram.cs
+++ Free Foundational C# with Microsoft Certification/Challenge project - Create a mini-game/MyProgram.cs
@@ -1,155 +1,3 @@
-// using System;
-
-// Random random = new Random();
-// Console.CursorVisible = false;
-// int height = Console.WindowHeight - 1;
-// int width = Console.WindowWidth - 5;
-// bool shouldExit = false;
-
-// // Console position of the player
-// int playerX = 0;
-// int playerY = 0;
-
-// // Console position of the food
-// int foodX = 0;
-// int foodY = 0;
-
-// // Available player and food strings
-// string[] states = {"('-')", "(^-^)", "(X_X)"};
-// string[] foods = {"@@@@@", "$$$$$", "#####"};
-
-// // Current player string displayed in the Console
-// string player = states[0];
-
-// // Index of the current food
-// int food = 0;
-
-// InitializeGame();
-
-// while (!shouldExit)
-// {
-//     if (TerminalResized())
-//     {
-//         Console.Clear();
-//         Console.WriteLine("Console was resized. Program exiting.");
-//         Thread.Sleep(1000);
-//         shouldExit = true;
-//     }
-//     else
-//     {
-//         Move(); // to enable optional parameter input 'true'
-//     }
-// }
-
-// // Returs true if food was eaten
-// bool AteFood(int playerX, int playerY, int foodX, int foodY)
-// {
-//     if (playerY == foodY)
-//     {
-//         for (int i = 0; i < player.Length; i++)
-//         {
-//             if (playerX == foodX)
-//                 return true;
-//         }
-//     }
-//     return false;
-// }
-
-// // Returns true if the Terminal was resized 
-// bool TerminalResized() 
-// {
-//     return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
-// }
-
-// // Displays random food at a random location
-// void ShowFood()
-// {
-//     // Update food to a random index
-//     food = random.Next(0, foods.Length);
-
-//     // Update food position to a random location
-//     foodX = random.Next(0, width - player.Length);
-//     foodY = random.Next(0, height - 1);
-
-//     // Display the food at the location
-//     Console.SetCursorPosition(foodX, foodY);
-//     Console.Write(foods[food]);
-// }
-
-// // Changes the player to match the food consumed
-//     void ChangePlayer() 
-// {
-//     player = states[food];
-//     Console.SetCursorPosition(playerX, playerY);
-//     Console.Write(player);
-// }
-
-// // Temporarily stops the player from moving
-// void FreezePlayer() 
-// {
-//     System.Threading.Thread.Sleep(1000);
-//     player = states[0];
-// }
-
-// // Reads directional input from the Console and moves the player
-// void Move(bool optionalParameter = false)
-// {
-//     int lastX = playerX;
-//     int lastY = playerY;
-
-//     switch (Console.ReadKey(true).Key)
-//     {
-//         case ConsoleKey.UpArrow:
-//             playerY--;
-//             break;
-//         case ConsoleKey.DownArrow:
-//             playerY++;
-//             break;
-//         case ConsoleKey.LeftArrow:
-//             playerX--;
-//             break;
-//         case ConsoleKey.RightArrow:
-//             playerX++;
-//             break;
-//         case ConsoleKey.Escape:
-//             shouldExit = true;
-//             break;
-//         // optional parameter to close game with any input but directional keys
-//         default:
-//             if (optionalParameter)
-//                 shouldExit = true;
-//             break;
-//     }
-
-//     // Clear the characters at the previous position
-//     Console.SetCursorPosition(lastX, lastY);
-//     for (int i = 0; i < player.Length; i++)
-//     {
-//         Console.Write(" ");
-//     }
-
-//     // Keep player position within the bounds of the Terminal window
-//     playerX = (playerX < 0) ? 0 : (playerX >= width ? width : playerX);
-//     playerY = (playerY < 0) ? 0 : (playerY >= height ? height : playerY);
-
-//     // Draw the player at the new location
-//     Console.SetCursorPosition(playerX, playerY);
-//     Console.Write(player);
-//     if (AteFood(playerX, playerY, foodX, foodY))
-//     {
-//         ShowFood();
-//     }
-// }
-
-// // Clears the console, displays the food and player
-// void InitializeGame() 
-// {
-//     Console.Clear();
-//     ShowFood();
-//     Console.SetCursorPosition(0, 0);
-//     Console.Write(player);
-// }
-
 using System;
 
 Random random = new Random();
@@ -188,7 +36,8 @@ while (!shouldExit)
         break;
     }
 
-    Move();
+    if (ShouldPlayerSpeedUp()) Move(exitGameWithOtherKeys: true, IncreasedMovementSpeed: true);
+    else Move(exitGameWithOtherKeys: true);
 
     if (ConsumedFood())
     {
@@ -243,6 +92,11 @@ void ChangePlayer() // local: player -- needs: food, playerX, playerY
     Console.Write(player);
 }
 
+bool ShouldPlayerSpeedUp()
+{
+    return (player == states[1]);
+}
+
 bool ShouldPlayerFreeze()
 {
     return (player == states[2]) ? true : false;
@@ -256,7 +110,7 @@ void FreezePlayer()
 }
 
 // Reads directional input from the Console and moves the player
-void Move(bool exitGameWithAnyKey = false, bool IncreasedMovementSpeed = false)
+void Move(bool exitGameWithOtherKeys = false, bool IncreasedMovementSpeed = false)
 {
     int lastX = playerX;
     int lastY = playerY; 
@@ -270,17 +124,17 @@ void Move(bool exitGameWithAnyKey = false, bool IncreasedMovementSpeed = false)
             playerY++;            
             break;
 		case ConsoleKey.LeftArrow:  
-            playerX = (IncreasedMovementSpeed && player == state[1]) ? (playerX - 4) : playerX--; 
+            playerX = (IncreasedMovementSpeed) ? (playerX - 4) : playerX--; 
             break;
 		case ConsoleKey.RightArrow: 
-            playerX = (IncreasedMovementSpeed && player == state[1]) ? (playerX + 4) : playerX++; 
+            playerX = (IncreasedMovementSpeed) ? (playerX + 4) : playerX++; 
             break;
 		case ConsoleKey.Escape:     
             shouldExit = true; 
             break;
         // optional parameter to close game with any input but directional keys
         default:
-            if (exitGameWithAnyKey)
+            if (exitGameWithOtherKeys)
                 shouldExit = true;
             break;
     }
